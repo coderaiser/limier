f824e7228436a837ad339c4656113e67f703bfe5
coderaiser <coderaiser@cloudcmd.io>
2018-03-02 11:28:32 +0200

    chore(limier) let -> const

--- test/limier.js
+++ test/limier.js
@@ -1,36 +1,36 @@
 'use strict';
 
-let limier  = require('../src/limier');
-let test    = require('tape');
+const limier = require('../src/limier');
+const test = require('tape');
 
-test('direct search', t => {
-    let result = limier('hello', ['hello', 'world']);
+test('direct search', (t) =>{
+    const result = limier('hello', ['hello', 'world']);
     
     t.deepEqual(result, ['hello']);
     t.end();
 });
 
-test('pattern search', t => {
-    let result = limier('bwiw', ['hello', 'biggestWordInAWorld']);
+test('pattern search', (t) =>{
+    const result = limier('bwiw', ['hello', 'biggestWordInAWorld']);
     
     t.deepEqual(result, ['biggestWordInAWorld']);
     t.end();
 });
 
-test('pattern search: no result', t => {
-    let result = limier('abc', ['hello', 'biggestWordInAWorld']);
+test('pattern search: no result', (t) =>{
+    const result = limier('abc', ['hello', 'biggestWordInAWorld']);
     
     t.deepEqual(result, []);
     t.end();
 });
 
-test('arguments: no', t => {
+test('arguments: no', (t) =>{
     t.throws(limier, /pattern should be string!/, 'should throw when no pattern');
     t.end();
 });
 
-test('arguments: no list', t => {
-    let fn = () => limier('hello');
+test('arguments: no list', (t) =>{
+    const fn = () => limier('hello');
     
     t.throws(fn, /list should be an array!/, 'should throw when no list');
     t.end();
